20b35e837b74aa64297562b609220e5008076a68
Yusra Khalid <yusrakhalid825@gmail.com>
2026-05-09 13:24:27 +0500

    distributed microservices complete - all services working

--- db.py
+++ db.py
@@ -3,10 +3,11 @@ import mysql.connector
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="12345678",
-            database="fashion_net"
+            host="fashion-net-2026-dc.mysql.database.azure.com",
+            user="yusrakhalid8",
+            password="Joakmn095",
+            database="fashion_net",
+            ssl_disabled=True
         )
         return connection
     except mysql.connector.Error as err:
