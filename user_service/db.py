2162a8dec0d38b0ad99158e9a621de9178e00999
Yusra Khalid <yusrakhalid825@gmail.com>
2026-05-14 16:15:14 +0500

    fixed localhost to azure db

--- user_service/db.py
+++ user_service/db.py
@@ -2,11 +2,15 @@ import mysql.connector
 import os
 
 def get_db_connection():
-    connection = mysql.connector.connect(
-        host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
-        database=os.environ.get("DB_NAME"),
-        ssl_disabled=False
-    )
-    return connection
+    try:
+        connection = mysql.connector.connect(
+            host=os.environ.get('DB_HOST', 'fashion-net-2026-dc.mysql.database.azure.com'),
+            user=os.environ.get('DB_USER', 'yusrakhalid8'),
+            password=os.environ.get('DB_PASSWORD', 'Joakmn095'),
+            database=os.environ.get('DB_NAME', 'fashion_net'),
+            ssl_disabled=True
+        )
+        return connection
+    except mysql.connector.Error as err:
+        print(f"Database connection error: {err}")
+        raise
