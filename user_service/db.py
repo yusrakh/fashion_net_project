3fcab9ddf345c9f52a4417569e1968f50bda3d03
Yusra Khalid <yusrakhalid825@gmail.com>
2026-05-15 14:33:08 +0500

    fixed username in db connection

--- user_service/db.py
+++ user_service/db.py
@@ -4,35 +4,16 @@ import os
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host=os.environ.get(
-                'DB_HOST',
-                'fashion-net-2026-dc.mysql.database.azure.com'
-            ),
-
-            user=os.environ.get(
-                'DB_USER',
-                'yusrakhalid8@fashion-net-2026-dc'
-            ),
-
-            password=os.environ.get(
-                'DB_PASSWORD',
-                'Joakmn095'
-            ),
-
-            database=os.environ.get(
-                'DB_NAME',
-                'fashion_net'
-            ),
-
+            host=os.environ.get('DB_HOST', 'fashion-net-2026-dc.mysql.database.azure.com'),
+            user=os.environ.get('DB_USER', 'yusrakhalid8'),
+            password=os.environ.get('DB_PASSWORD', 'Joakmn095'),
+            database=os.environ.get('DB_NAME', 'fashion_net'),
             port=3306,
-
             ssl_disabled=False,
             ssl_verify_cert=False,
             ssl_verify_identity=False
         )
-
         return connection
-
     except mysql.connector.Error as err:
         print(f"Database connection error: {err}")
         raise
